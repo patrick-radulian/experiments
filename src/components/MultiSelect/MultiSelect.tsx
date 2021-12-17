030ec8226e815ce3b5f014729f924d7b4bba726d
Patrick Radulian <patrick.radulian@gmail.com>
2021-12-17 19:35:40 +0100

    Add logic to display "multiple selected" chip

--- src/components/MultiSelect/MultiSelect.tsx
+++ src/components/MultiSelect/MultiSelect.tsx
@@ -7,12 +7,14 @@ import UnfoldMoreIcon from '@mui/icons-material/UnfoldMore';
 
 export default function MultiSelect() {
     const [open, setOpen] = React.useState(false);
+    const [bodyFull, setBodyFull] = React.useState(false);
     const [checkedOptions, setChecked] = React.useState<Array<IMultiSelectOption>>([]);
 
+    const multiSelect = React.createRef<HTMLDivElement>();
+    const multiSelectBody = React.createRef<HTMLDivElement>();
+
     const handleClickOpen = () => setOpen(true);
-    const handleClose = () => {
-        setOpen(false);
-    }
+    const handleClose = () => setOpen(false);
 
     const handleToggle = (option: IMultiSelectOption) => {
         const currentIndex = checkedOptions.indexOf(option);
@@ -28,18 +30,23 @@ export default function MultiSelect() {
     };
 
     React.useEffect(() => {
-        console.log("processing side-effect");
+        if (!multiSelectBody.current || !multiSelect.current) return;
+        setBodyFull((multiSelectBody.current.offsetWidth + 38) > multiSelect.current.clientWidth);
     }, [open]);
 
     return (
         <div className={styles.container}>
             <label className={styles["multi-select-label"]}>Group members</label>
 
-            <div className={styles["multi-select"]} onClick={handleClickOpen}>
-                <div className={styles["multi-select-body"]}>
-                    {checkedOptions.map(option => (
-                        <Chip className={styles["multi-select-chip"]} label={option.label} size="small" key={option.value}/>
-                    ))}
+            <div ref={multiSelect} className={styles["multi-select"]} onClick={handleClickOpen}>
+                <div ref={multiSelectBody} className={styles["multi-select-body"]}>
+                    {bodyFull ? (
+                        <Chip className={styles["multi-select-chip"]} label="Multiple Options Selected" size="small"/>
+                    ) : (
+                        checkedOptions.map(option => (
+                            <Chip className={styles["multi-select-chip"]} label={option.label} size="small" key={option.value}/>
+                        ))
+                    )}
                 </div>
 
                 <button className={styles["multi-select-expand-button"]}><UnfoldMoreIcon fontSize="medium"/></button>
