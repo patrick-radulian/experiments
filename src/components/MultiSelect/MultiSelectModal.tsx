1684454cc4cb4338bc0b6e4f0d31f8c5b9b6fc18
Patrick Radulian <patrick.radulian@gmail.com>
2022-01-11 14:08:43 +0100

    Add options filter input

--- src/components/MultiSelect/MultiSelectModal.tsx
+++ src/components/MultiSelect/MultiSelectModal.tsx
@@ -4,7 +4,9 @@ import Dialog from '@mui/material/Dialog';
 import DialogActions from '@mui/material/DialogActions';
 import DialogContent from '@mui/material/DialogContent';
 import DialogTitle from '@mui/material/DialogTitle';
-import { Checkbox, Grid, Switch, FormControlLabel } from "@mui/material";
+import { Checkbox, Grid, Switch, FormControlLabel, TextField, Box, InputAdornment, IconButton, Container } from "@mui/material";
+import ClearIcon from "@mui/icons-material/Clear";
+import SentimentDissatisfiedIcon from '@mui/icons-material/SentimentDissatisfied';
 
 export interface IMultiSelectOption {
     label: string,
@@ -32,36 +34,67 @@ export default function MultiSelectModal(props: IMultiSelectModal) {
 
     const [useManyOptions, setUseManyOptions] = React.useState<boolean>(false);
     const [optionsToUse, setOptionsToUse] = React.useState<Array<IMultiSelectOption>>(options.fewOptions);
+    const [optionsFilterValue, setOptionsFilterValue] = React.useState<string>("");
+    const [filteredOptions, setFilteredOptions] = React.useState<Array<IMultiSelectOption>>([]);
 
     const handleSwitch = (event: React.ChangeEvent<HTMLInputElement>, checked: boolean) => {
         setUseManyOptions(checked);
         setOptionsToUse(checked ? options.manyOptions : options.fewOptions);
     }
 
+    const handleFilterChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+        setOptionsFilterValue(event.target.value);
+    }
+
+    const FilterFieldAdornment = () => {
+        return (
+            <InputAdornment position="end">
+                <IconButton onClick={() => setOptionsFilterValue("")}>
+                    <ClearIcon/>
+                </IconButton>
+            </InputAdornment>
+        )
+    }
+
+    React.useEffect(() => {
+        setFilteredOptions(() => optionsToUse.filter(option => option.label.toLowerCase().includes(optionsFilterValue.toLowerCase())));
+    }, [optionsFilterValue, optionsToUse]);
+
     return (
-        <Dialog open={open} onClose={handleClose} maxWidth="lg">
+        <Dialog open={open} onClose={handleClose} maxWidth="lg" fullWidth>
             <DialogTitle sx={{display: "flex", justifyContent: "space-between"}}>
                 <span>{title}</span>
                 <FormControlLabel control={<Switch checked={useManyOptions} onChange={handleSwitch}/>} label="Use Many Options" />
             </DialogTitle>
 
             <DialogContent dividers>
+                <Box sx={{marginBottom: "1rem"}}>
+                    <TextField fullWidth value={optionsFilterValue} onChange={handleFilterChange} label="Filter Options" variant="outlined" InputProps={{endAdornment: <FilterFieldAdornment/>}} />
+                </Box>
+
                 <Grid container spacing={2}>
-                    {optionsToUse.map(option => (
-                        <Grid item xs={4} key={option.value}>
-                            <FormControlLabel
-                                control={
-                                    <Checkbox checked={checkedOptions.indexOf(option) !== -1}
-                                        onChange={onChange(option)}
-                                        tabIndex={-1}
-                                        name={option.value}
-                                        disableRipple
-                                    />
-                                }
-                                label={option.label}
-                            />
-                        </Grid>
-                    ))}
+                    {filteredOptions.length > 0 ? (
+                        filteredOptions.map(option => (
+                            <Grid item xs={4} key={option.value}>
+                                <FormControlLabel
+                                    control={
+                                        <Checkbox checked={checkedOptions.indexOf(option) !== -1}
+                                            onChange={onChange(option)}
+                                            tabIndex={-1}
+                                            name={option.value}
+                                            disableRipple
+                                        />
+                                    }
+                                    label={option.label}
+                                />
+                            </Grid>
+                        ))
+                    ) : (
+                        <Container fixed sx={{display: "flex", flexDirection: "column", alignItems: "center", p: "1rem", fontSize: "1.15rem", opacity: 0.7}} >
+                            <span>No matching options</span>
+                            <SentimentDissatisfiedIcon fontSize="large" sx={{mt: "1rem"}}/>
+                        </Container>
+                    )}
                 </Grid>
             </DialogContent>
 
