51c8991efbeba04f6f0f442efa709f83ab36565b
Patrick Radulian <patrick.radulian@gmail.com>
2022-01-27 10:39:23 +0100

    streamline other-experiements style

--- src/routes/other-experiments/other-experiments.tsx
+++ src/routes/other-experiments/other-experiments.tsx
@@ -1,24 +1,31 @@
-import { Box, Grid, Paper, styled } from "@mui/material";
-
-const Item = styled(Paper)(({ theme }) => ({
-    ...theme.typography.body2,
-    padding: theme.spacing(1),
-    textAlign: 'center',
-    color: theme.palette.text.secondary,
-}));
+import { Box, Card, CardContent, Container, Grid, Link } from "@mui/material";
+import { Link as RouterLink } from "react-router-dom";
+import styles from "./other-experiments.module.css";
 
 const OtherExperiments = () => {
     return (
-        <Box maxWidth={782} mt={-6} mx="auto">
+        <Box maxWidth={782} my={8} mx="auto">
             <Grid container spacing={2}>
                 <Grid item md={4} sm={6} xs={12}>
                     <Box display="flex" justifyContent="center">
-                        <Item sx={{height: 250, width: 250}}>xs=4</Item>
+                        <Link component={RouterLink} to="selects" underline="hover">
+                            <Card className={styles.card}>
+                                <CardContent>
+                                    <Container>Selects</Container>
+                                </CardContent>
+                            </Card>
+                        </Link>
                     </Box>
                 </Grid>
                 <Grid item md={4} sm={6} xs={12}>
                     <Box display="flex" justifyContent="center">
-                        <Item sx={{height: 250, width: 250}}>xs=4</Item>
+                        <Link component={RouterLink} to="selects" underline="hover">
+                            <Card className={styles.card}>
+                                <CardContent>
+                                    <Container>Selects</Container>
+                                </CardContent>
+                            </Card>
+                        </Link>
                     </Box>
                 </Grid>
             </Grid>
