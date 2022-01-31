5a4cc520898dff31a1c124d7c34e3beccae6b5c5
Patrick Radulian <patrick.radulian@gmail.com>
2022-01-31 10:08:05 +0100

    Order priviledge mock-data alphabetically

--- src/mock-data/priviledges.ts
+++ src/mock-data/priviledges.ts
@@ -4,35 +4,35 @@ export const priviledges = new MultiSelectOptionGroups([
     {
         label: "Classifier",
         options: [
-            {label: "Delete classifier instances", value: "classifier_delete"},
-            {label: "Create and edit classifiers", value: "classifier_create_edit"},
-            {label: "Start classification jobs", value: "classifier_start_jobs"},
             {label: "Access the classifier", value: "classifier_access"},
+            {label: "Create and edit classifiers", value: "classifier_create_edit"},
+            {label: "Delete classifier instances", value: "classifier_delete"},
+            {label: "Start classification jobs", value: "classifier_start_jobs"},
         ]
     },
     {
         label: "Project",
         options: [
+            {label: "Access projects", value: "project_access"},
             {label: "Create and edit project data", value: "project_create_edit"},
             {label: "Delete project data", value: "project_delete"},
-            {label: "Access projects", value: "project_access"},
         ]
     },
     {
         label: "Corpus",
         options: [
-            {label: "Delete corpora", value: "corpus_delete"},
-            {label: "Create and edit corpora", value: "corpus_create_edit"},
             {label: "Access the corpus management", value: "corpus_access"},
+            {label: "Create and edit corpora", value: "corpus_create_edit"},
+            {label: "Delete corpora", value: "corpus_delete"},
         ]
     },
     {
         label: "Workflow",
         options: [
-            {label: "Assign items in the Workflow Management", value: "workflow_assign"},
             {label: "Approve Workflow items", value: "workflow_approve"},
-            {label: "Reject Workflow items", value: "workflow_reject"},
+            {label: "Assign items in the Workflow Management", value: "workflow_assign"},
             {label: "Edit the Workflow settings", value: "workflow_edit"},
+            {label: "Reject Workflow items", value: "workflow_reject"},
         ]
     },
 ])
